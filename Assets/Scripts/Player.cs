af1419d7a9d2bcffa75f7654cb3f52bd395ea8f7
fierval <fierval@gmail.com>
2019-05-04 23:57:52 -0700

    fix animation not loooping

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 30;
+    Rigidbody2D rb;
+    Vector2 moveAmount;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+
+        anim.SetBool("isRunning", moveInput != Vector2.zero);
+        if (anim.GetBool("isRunning"))
+        {
+            Debug.Log($"isRunning: {anim.GetBool("isRunning")}");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
+    }
+}
