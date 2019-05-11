8931409cc0f4243a1ed6ceede4f62fc59868fd3f
fierval <fierval@gmail.com>
2019-05-11 13:18:11 -0700

    shooting and partlicles

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+
+    public GameObject explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+        Instantiate(explosion, transform.position, Quaternion.identity);
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public GameObject projectile;
+    public Transform shotPoint;
+    public float timeBetweenShots;
+
+    float shotTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        var direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+
+        if (Input.GetMouseButton(0) && Time.time >= shotTime)
+        {
+            Instantiate(projectile, shotPoint.position, transform.rotation);
+            shotTime = Time.time + timeBetweenShots;
+        }
     }
 }
