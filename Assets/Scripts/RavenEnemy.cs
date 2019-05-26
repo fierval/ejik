2e044b4cef67b7919a070f16ea1faaa4076a43dc
fierval <fierval@gmail.com>
2019-05-25 18:17:41 -0700

    make raven face player

--- Assets/Scripts/RavenEnemy.cs
+++ Assets/Scripts/RavenEnemy.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class RavenEnemy : Enemy
 {
+    bool facingRight = true;
+
+    protected override void Start()
+    {
+        base.Start();
+        FaceTowardsPlayer();
+    }
     // Start is called before the first frame update
     // Update is called once per frame
     protected override void Update()
     {
         base.Update();
+        FaceTowardsPlayer();
+    }
+
+    void FaceTowardsPlayer()
+    {
+        if (player == null) { return; }
+        var xDir = player.position.x - transform.position.x;
+
+        if (xDir > 0 && !facingRight || xDir < 0 && facingRight)
+        {
+            transform.eulerAngles = 
+                new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 180f, transform.eulerAngles.z);
+            facingRight = !facingRight;
+        }
     }
 
 }
