1dbcec071fe6da616c73c55f7c90a5b20138cf79
fierval <fierval@gmail.com>
2019-05-30 01:28:53 -0700

    redo correct player management

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,7 +7,15 @@ public class PlayerManager : MonoBehaviour
     public static PlayerManager Instance { get; private set; }
     private void Awake()
     {
-        Instance = this;
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     public GameObject player;
