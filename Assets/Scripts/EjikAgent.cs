91ba3ad64f87411ba9c2d950f614e81c39280bfb
fierval <fierval@gmail.com>
2019-06-15 11:46:16 -0700

    add components

---
+++ Assets/Scripts/EjikAgent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using MLAgents.CommunicatorObjects;
+
+public class EjikAgent : Agent
+{
+    Player ejik;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ejik = GetComponent<Player>();      
+    }
+
+    public override void AgentReset()
+    {
+        if (ejik.health <= 0)
+        {
+            ejik.gameObject.transform.position = Vector3.zero;
+            ejik.health = 1f;
+        }
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction, CustomAction customAction)
+    {
+        base.AgentAction(vectorAction, textAction, customAction);
+    }
+}
