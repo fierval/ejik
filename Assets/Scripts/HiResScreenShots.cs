579b2dfeb22829ffb78390efe9dffb85b18b9b9b
fierval <fierval@gmail.com>
2019-06-16 12:01:14 -0700

    looking at camera resolutions

---
+++ Assets/Scripts/HiResScreenShots.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiResScreenShots : MonoBehaviour
+{
+    public int resWidth = 500;
+    public int resHeight = 400;
+
+    private bool takeHiResShot = false;
+    new Camera camera;
+
+    private void Start()
+    {
+        camera = gameObject.GetComponentInChildren<Camera>();
+    }
+
+    public static string ScreenShotName(int width, int height)
+    {
+        return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png",
+                             Application.dataPath,
+                             width, height,
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    public void TakeHiResShot()
+    {
+        takeHiResShot = true;
+    }
+
+    void LateUpdate()
+    {
+        takeHiResShot |= Input.GetKeyDown("k");
+        if (takeHiResShot)
+        {
+            camera.gameObject.SetActive(true);
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+            camera.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            camera.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            camera.targetTexture = null;
+            RenderTexture.active = Camera.main.targetTexture; // JC: added to avoid errors
+            Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filename = ScreenShotName(resWidth, resHeight);
+            System.IO.File.WriteAllBytes(filename, bytes);
+            Debug.Log(string.Format("Took screenshot to: {0}", filename));
+            takeHiResShot = false;
+            camera.gameObject.SetActive(false);
+        }
+    }
+}
