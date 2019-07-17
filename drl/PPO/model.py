5e478d93d1f91f69ada13bb6dbaa491e1f0f31fc
fierval <fierval@gmail.com>
2019-07-17 14:27:24 -0700

    model

--- drl/PPO/model.py
+++ drl/PPO/model.py
@@ -1,54 +1,65 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 HID_SIZE = 512
 
 def xavier(sequential):
-    for layer in sequential:
-        if isinstance(layer, nn.Linear):
-            nn.init.xavier_uniform_(layer.weight.data)
+    if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
+        nn.init.xavier_uniform_(layer.weight)
 
-def hidden_layers(obs_size, hid_size, hid_size_1):
-    return nn.Sequential(
-            nn.Linear(obs_size, hid_size),
-            nn.LeakyReLU(),
-            nn.Linear(hid_size, hid_size_1),
-            nn.LeakyReLU(),
-            )
-
-class GaussianPolicyActorCritic(nn.Module):
+class ActorCritic(nn.Module):
     def __init__(self, obs_size, act_size):
+        '''
+        obs_size - (C, H, W) tuple of a visual observation
+        act_size - action space size
+        '''
         super().__init__()
 
         self.action_dim = act_size
         self.state_dim = obs_size
-
-        hid_size = HID_SIZE
-        hid_size_1 = HID_SIZE // 2
-
-        self.fc_hidden = hidden_layers(obs_size, hid_size, hid_size_1)
         
-        self.fc_actor = nn.Linear(hid_size_1, act_size)
-        # value function
-        self.fc_critic = nn.Linear(hid_size_1, 1)
+        conv_size = self.get_conv_out()
+        fc_critic = self.hidden_layers() + [nn.Linear(conv_size, 1)]
+        fc_actor = self.hidden_layers() + [nn.LeakyReLU(), nn.Linear(conv_size, self.action_dim), nn.Tanh()]
+        
+        self.actor = nn.Sequential(*fc_actor)
+        self.critic = nn.Sequential(*fc_critic)
 
-        xavier(self.fc_hidden)                
-        self.std = nn.Parameter(torch.zeros(act_size))
+        self.init_weights()
 
-    def forward(self, states, actions=None):
-        phi = self.fc_hidden(states)
-        mu = torch.tanh(self.fc_actor(phi))
-        value = self.fc_critic(phi).squeeze(-1)
+        self.log_std = nn.Parameter(torch.zeros(act_size))
 
-        dist = torch.distributions.Normal(mu, F.softplus(self.std))
+    def init_weights(self):
+        self.actor.apply(xavier)
+        self.critic.apply(xavier)
+
+    def hidden_layers(self):
+        return [
+            nn.Conv2d(self.state_dim[0], 32, 8, stride=8),
+            nn.LeakyReLU(),
+            nn.Conv2d(32, 64, 4, stride=4),
+            nn.LeakyReLU(),
+            nn.Conv2d(64, 64, 3, stride=2)
+        ]
+
+    def get_conv_out(self):
+        o = nn.Sequential(*self.hidden_layers())(torch.zeros(1, *self.state_dim))
+        return int(np.prod(o.size()))
+
+    def forward(self, x, actions=None):
+        mu = self.fc_actor(x)
+        value = self.critic(x)
+
+        std = self.log_std.exp().expand_as(mu)
+        dist = torch.distributions.Normal(mu, std)
+
         if actions is None:
             actions = dist.sample()
-        log_prob = dist.log_prob(actions)
-        log_prob = torch.sum(log_prob, dim=-1)
-        entropy = torch.sum(dist.entropy(), dim=-1)
+        log_prob = dist.log_prob(actions).mean()
+        entropy = dist.entropy().mean()
         return actions, log_prob, entropy, value
 
     def state_values(self, states):
-        phi = self.fc_hidden(states)
-        return self.fc_critic(phi).squeeze(-1)
+        return self.critic(states)
