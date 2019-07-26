a425ef0000ba618ab33497676570ccc4fb0eaa77
boris <fierval@gmail.com>
2019-07-26 14:52:07 -0700

    add about

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void About()
+    {
+        SceneManager.LoadScene("About");
+    }
+
     public void QuitGame()
     {
         Application.Quit();
